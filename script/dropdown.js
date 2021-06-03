884fb2b7a535aac141a835b6608779757f98196a
Dilyara <mardanovadiliara@yandex.ru>
2021-06-03 15:00:28 +0300

    error correction

--- script/dropdown.js
+++ script/dropdown.js
@@ -1,24 +1,41 @@
-document.addEventListener('DOMContentLoaded', function () {
-    const btnMenu = document.querySelectorAll('.header__bottom-link');
-    const menu = document.querySelectorAll('.dropdown');
+// document.addEventListener('DOMContentLoaded', function () {
+//     const btnMenu = document.querySelectorAll('.header__bottom-btn');
+//     const menu = document.querySelectorAll('.dropdown');
 
-        btnMenu.forEach(function(drop) {
-            drop.addEventListener('click', function(event) {
+//         btnMenu.forEach(function(drop) {
+//             drop.addEventListener('click', function(event) {
 
-            const path = event.currentTarget.dataset.path
+//             const path = event.currentTarget.dataset.path
 
-            menu.forEach(function(dropContent) {
-                dropContent.classList.remove('.dropdown-active')
-            })
+//             menu.forEach(function(dropContent) {
+//                 dropContent.classList.remove('.dropdown-active')
+//             })
 
-            document.querySelector(`[data-target="${path}"]`).classList.toggle('dropdown-active')
-        })
+//             document.querySelector(`[data-target="${path}"]`).classList.toggle('dropdown-active')
+//         })
 
-        document.addEventListener('click', function(e) {
-            console.log(e.target)
-            if (!e.target.classList.contains('dropdown') && !e.target.classList.contains('header__bottom-link')) {
-              menu.forEach(el => {el.classList.remove(('dropdown-active'))})
-            }
-        })
-    })
-})
+//         document.addEventListener('click', function(e) {
+//             console.log(e.target)
+//             if (!e.target.classList.contains('dropdown') && !e.target.classList.contains('header__bottom-btn')) {
+//               menu.forEach(el => {el.classList.remove(('dropdown-active'))})
+//             }
+//         })
+//     })
+// })
+
+const button = document.querySelectorAll('.header__bottom-btn');
+const drop = document.querySelectorAll('.dropdown')
+
+button.forEach(el => {
+  el.addEventListener('click', (e) => {
+    drop.forEach(el => {el.classList.remove(('dropdown-active'))})
+    e.currentTarget.closest('li').querySelector('.dropdown').classList.toggle('dropdown-active');
+  });
+});
+
+document.addEventListener('click', (e) => {
+  console.log(e.target)
+  if (!e.target.classList.contains('dropdown') && !e.target.classList.contains('.header__bottom-btn')) {
+    drop.forEach(el => {el.classList.remove(('dropdown-active'))})
+  }
+});
