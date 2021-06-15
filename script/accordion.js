be274336ccafaf42e091532d3e8acaf93ebfa0ed
Dilyara <mardanovadiliara@yandex.ru>
2021-06-15 17:53:01 +0300

    accordion options correct

--- script/accordion.js
+++ script/accordion.js
@@ -1,8 +1,8 @@
 $( function() {
   $( ".accordion" ).accordion({
     collapsible: true,
-    active: 1
-
+    active: 1,
+    heightStyle: "content"
   });
   $( ".accordion" ).accordion( "option", "active", 1 );
 } );
