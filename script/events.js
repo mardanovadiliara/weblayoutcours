a3fde4987f2f45b7ea7bccec85517ecaca249ffe
Dilyara <mardanovadiliara@yandex.ru>
2021-07-05 16:21:29 +0300

    events block added

---
+++ script/events.js
@@ -0,0 +1,46 @@
+document.addEventListener('DOMContentLoaded', function () {
+  const btn = document.querySelector('.event__more-btn');
+
+  btn.addEventListener('click', function () {
+    document.querySelectorAll('.event__item').forEach(function(openAll) {
+      openAll.style.display = "block"
+    })
+    })
+  })
+
+ const slider = document.querySelector('.swiper-container1')
+
+let mySwiper;
+
+function mobileSlider() {
+  if (window.innerWidth <= 767 && slider.dataset.mobile == 'false') {
+    mySwiper = new Swiper(slider, {
+      slidePerView: 1,
+      slideClass: 'event__item',
+      wrapperClass: 'swiper-wrapper1',
+      pagination: {
+        el:'swiper-pagination1',
+        type: 'bullets',
+      }
+    });
+
+    slider.dataset.mobile = 'true';
+  }
+
+  if (window.innerWidth > 767) {
+    slider.dataset.mobile = 'false';
+
+    if (slider.classList.contains('swiper-container-initialized')) {
+      mySwiper.destroy();
+    }
+  }
+}
+
+mobileSlider();
+
+window.addEventListener('resize', () => {
+  mobileSlider();
+});
+
+
+
