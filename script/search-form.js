65980729d0c02c3fe9df87155bd65c3e4c39d371
Dilyara <mardanovadiliara@yandex.ru>
2021-11-06 21:07:19 +0300

    search form errors correction

--- script/search-form.js
+++ script/search-form.js
@@ -1,11 +1,9 @@
 document.addEventListener('DOMContentLoaded', function () {
-
   const openBtn = document.querySelector ('.open-button');
   const searchBtn = document.querySelector ('.search-button');
   const form = document.querySelector ('.header__form-mobile');
   const closeFormBtn = document.querySelector ('.form__close-btn');
-
-  let isOpened = false;
+      let isOpened = false;
 
   openBtn.addEventListener('click', function(e) {
     e.preventDefault();
@@ -20,26 +18,26 @@ document.addEventListener('DOMContentLoaded', function () {
 
     form.classList.add('open-form');
 
-    isOpened = true;
+     isOpened = true;
   })
 
   closeFormBtn.addEventListener('click', function(e) {
-    e.preventDefault();
+      e.preventDefault();
 
-    form.classList.remove('open-form');
+      form.classList.remove('open-form');
 
-    isOpened = false;
+     isOpened = false;
   })
-});
 
   document.addEventListener('click', function(e) {
+
     console.log(e.target.classList.contains('header__form-mobile'))
 
     console.log(e.target.closest('.header__form-mobile'))
 
     if (!e.target.classList.contains('header__form-mobile') && !e.target.classList.contains('open-button') && !e.target.closest('.header__form-mobile') && isOpened) {
-      form.classList.remove('open-form');
+      form.classList.remove('open-form')
     }
   });
-
+})
 
